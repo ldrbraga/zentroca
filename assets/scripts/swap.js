abd5f9834d238a8ddd47857a25d20ddff30d362d
Leandro Braga <ldrbraga@gmail.com>
2021-06-15 15:46:40 -0300

    Create zentroca-new section

--- assets/scripts/swap.js
+++ assets/scripts/swap.js
@@ -3,6 +3,11 @@ const physicalLink = $('.zentroca-participate__infos__links--physical');
 const onlineContent = $('.zentroca-participate__infos__content--online');
 const physicalContent = $('.zentroca-participate__infos__content--physical');
 
+const onlineLinkNew = $('.zentroca-new__box__content__links--online');
+const physicalLinkNew = $('.zentroca-new__box__content__links--physical');
+const onlineContentNew = $('.zentroca-new__box__content__form');
+const physicalContentNew = $('.zentroca-new__box__content__result');
+
 onlineLink.click((e) => {
   e.preventDefault();
   
@@ -23,5 +28,26 @@ physicalLink.click((e) => {
   
   onlineLink.removeClass('active');
   physicalLink.addClass('active');
+});
 
+onlineLinkNew.click((e) => {
+  e.preventDefault();
+  
+  onlineContentNew.css('display', 'block');
+  physicalContentNew.css('display', 'none');
+  
+  onlineLinkNew.addClass('active');
+  physicalLinkNew.removeClass('active');
+});
+
+physicalLinkNew.click((e) => {
+  e.preventDefault();
+
+  $(physicalContentNew).fadeToggle(500);
+
+  onlineContentNew.css('display', 'none');
+  physicalContentNew.css('display', 'block');
+  
+  onlineLinkNew.removeClass('active');
+  physicalLinkNew.addClass('active');
 });
--- public/assets/scripts/swap-debug.js
+++ public/assets/scripts/swap-debug.js
@@ -4,6 +4,10 @@ var onlineLink = $('.zentroca-participate__infos__links--online');
 var physicalLink = $('.zentroca-participate__infos__links--physical');
 var onlineContent = $('.zentroca-participate__infos__content--online');
 var physicalContent = $('.zentroca-participate__infos__content--physical');
+var onlineLinkNew = $('.zentroca-new__box__content__links--online');
+var physicalLinkNew = $('.zentroca-new__box__content__links--physical');
+var onlineContentNew = $('.zentroca-new__box__content__form');
+var physicalContentNew = $('.zentroca-new__box__content__result');
 onlineLink.click(function (e) {
   e.preventDefault();
   onlineContent.css('display', 'block');
@@ -18,4 +22,19 @@ physicalLink.click(function (e) {
   physicalContent.css('display', 'block');
   onlineLink.removeClass('active');
   physicalLink.addClass('active');
+});
+onlineLinkNew.click(function (e) {
+  e.preventDefault();
+  onlineContentNew.css('display', 'block');
+  physicalContentNew.css('display', 'none');
+  onlineLinkNew.addClass('active');
+  physicalLinkNew.removeClass('active');
+});
+physicalLinkNew.click(function (e) {
+  e.preventDefault();
+  $(physicalContentNew).fadeToggle(500);
+  onlineContentNew.css('display', 'none');
+  physicalContentNew.css('display', 'block');
+  onlineLinkNew.removeClass('active');
+  physicalLinkNew.addClass('active');
 });
